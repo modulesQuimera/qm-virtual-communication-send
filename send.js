67755d56348e322db302ca9051ab0684dbeb63c8
Ian Alves <ian.alves21@gmail.com>
2020-07-29 19:01:26 -0400

    update

--- send.js
+++ send.js
@@ -1,7 +1,37 @@
 module.exports = function(RED) {
 
-    "use strict";
     var mapeamentoNode;
+
+    function multipleSend(self, file, slot, currentMode){
+        for(var t=0; t<self.qtdSend; t++){
+            var command_n={
+                type: "communication_modular_V1_0",
+                slot: parseInt(mapeamentoNode.slot),
+                method: "send",
+                port_send: self.port_send_n[t],
+                message_send: self.message_send_n[t],
+                get_output: {},
+                compare: {}
+            }
+            if(!(slot === "begin" || slot === "end")){
+                if(currentMode == "test"){
+                    file.slots[slot].jig_test.push(command_n);
+                }
+                else{
+                    file.slots[slot].jig_error.push(command_n);
+                }
+            }
+            else{
+                if(slot === "begin"){
+                    file.slots[0].jig_test.push(command_n);
+                }
+                else{
+                    file.slots[3].jig_test.push(command_n);
+                }
+            }
+        }
+        return file;
+    }
 
     function sendNode(config) {
         RED.nodes.createNode(this, config);
@@ -9,6 +39,35 @@ module.exports = function(RED) {
         this.message_send = config.message_send
         this.port_send = config.port_send
         mapeamentoNode = RED.nodes.getNode(this.mapeamento);
+
+        this.qtdSend = config.qtdSend;
+        this.message_send_n = []; this.port_send_n = []; 
+        this.message_send_n.push(config.message_send1); this.port_send_n.push(config.port_send1);
+        this.message_send_n.push(config.message_send2); this.port_send_n.push(config.port_send2);
+        this.message_send_n.push(config.message_send3); this.port_send_n.push(config.port_send3);
+        this.message_send_n.push(config.message_send4); this.port_send_n.push(config.port_send4);
+        this.message_send_n.push(config.message_send5); this.port_send_n.push(config.port_send5);
+        this.message_send_n.push(config.message_send6); this.port_send_n.push(config.port_send6);
+        this.message_send_n.push(config.message_send7); this.port_send_n.push(config.port_send7);
+        this.message_send_n.push(config.message_send8); this.port_send_n.push(config.port_send8);
+        this.message_send_n.push(config.message_send9); this.port_send_n.push(config.port_send9);
+        this.message_send_n.push(config.message_send10); this.port_send_n.push(config.port_send10);
+        this.message_send_n.push(config.message_send11); this.port_send_n.push(config.port_send11);
+        this.message_send_n.push(config.message_send12); this.port_send_n.push(config.port_send12);
+        this.message_send_n.push(config.message_send13); this.port_send_n.push(config.port_send13);
+        this.message_send_n.push(config.message_send14); this.port_send_n.push(config.port_send14);
+        this.message_send_n.push(config.message_send15); this.port_send_n.push(config.port_send15);
+        this.message_send_n.push(config.message_send16); this.port_send_n.push(config.port_send16);
+        this.message_send_n.push(config.message_send17); this.port_send_n.push(config.port_send17);
+        this.message_send_n.push(config.message_send18); this.port_send_n.push(config.port_send18);
+        this.message_send_n.push(config.message_send19); this.port_send_n.push(config.port_send19);
+        this.message_send_n.push(config.message_send20); this.port_send_n.push(config.port_send20);
+        this.message_send_n.push(config.message_send21); this.port_send_n.push(config.port_send21);
+        this.message_send_n.push(config.message_send22); this.port_send_n.push(config.port_send22);
+        this.message_send_n.push(config.message_send23); this.port_send_n.push(config.port_send23);
+        this.message_send_n.push(config.message_send24); this.port_send_n.push(config.port_send24);
+
+
         var node = this
         
         node.on('input', function(msg, send, done) {
@@ -31,19 +90,21 @@ module.exports = function(RED) {
             if(!(slot === "begin" || slot === "end")){
                 if(currentMode == "test"){
                     file.slots[slot].jig_test.push(command);
+                    file = multipleSend(node, file, slot, currentMode)
                 }
                 else{
                     file.slots[slot].jig_error.push(command);
+                    file = multipleSend(node, file, slot, currentMode);
                 }
             }
             else{
                 if(slot === "begin"){
                     file.slots[0].jig_test.push(command);
-                    // file.begin.push(command);
+                    file = multipleSend(node, file, slot, currentMode)
                 }
                 else{
                     file.slots[3].jig_test.push(command);
-                    // file.end.push(command);
+                    file = multipleSend(node, file, slot, currentMode)
                 }
             }
             globalContext.set("exportFile", file);
@@ -52,35 +113,4 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("send", sendNode);
-
-    // RED.httpAdmin.get("/getMapeamento",function(req,res) {
-    //     if(mapeamentoNode){
-    //         res.json([
-    //             {value:mapeamentoNode.valuePort1, label:"RS232_DB9 -" + mapeamentoNode.labelPort1, hasValue:false},
-    //             {value:mapeamentoNode.valuePort2, label:"RS232_RJ11 -" + mapeamentoNode.labelPort2, hasValue:false},
-    //             {value:mapeamentoNode.valuePort3, label:"RS485 -" + mapeamentoNode.labelPort3, hasValue:false},
-    //             {value:mapeamentoNode.valuePort4, label:"UART_COM -" + mapeamentoNode.labelPort4, hasValue:false},
-    //             {value:mapeamentoNode.valuePort5, label:"UART_1 -" + mapeamentoNode.labelPort5, hasValue:false},
-    //             {value:mapeamentoNode.valuePort6, label:"UART_2 -" + mapeamentoNode.labelPort6, hasValue:false},
-    //             {value:mapeamentoNode.valuePort7, label:"UART_3 -" + mapeamentoNode.labelPort7, hasValue:false},
-    //             {value:mapeamentoNode.valuePort8, label:"UART_4 -" + mapeamentoNode.labelPort8, hasValue:false},
-    //             {value:mapeamentoNode.valuePort9, label:"I2C -" + mapeamentoNode.labelPort9, hasValue:false},
-    //             {value:mapeamentoNode.valuePort10, label:"SPI -" + mapeamentoNode.labelPort10, hasValue:false},
-    //         ])
-    //     }
-    //     else{
-    //         res.json([
-    //             {value:"RS232_1", label:"RS232_DB9 -", hasValue:false},
-    //             {value:"RS232_2", label:"RS232_RJ11 -", hasValue:false},
-    //             {value:"RS485", label:"RS485 -", hasValue:false},
-    //             {value:"UART_COM", label:"UART_COM -", hasValue:false},
-    //             {value:"UART_1", label:"UART_1 -", hasValue:false},
-    //             {value:"UART_2", label:"UART_2 -", hasValue:false},
-    //             {value:"UART_3", label:"UART_3 -", hasValue:false},
-    //             {value:"UART_4", label:"UART_4 -", hasValue:false},
-    //             {value:"I2C", label:"I2C -", hasValue:false},
-    //             {value:"SPI", label:"SPI -", hasValue:false},
-    //         ])
-    //     }
-    // });
 }
